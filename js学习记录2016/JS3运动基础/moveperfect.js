c0a4f21397b57848afbfc09b8717c3c3d4acd169
paopaor <1023981180@qq.com>
2017-08-16 17:02:16 +0800

    paopao
    js学习记录整理

---
+++ js学习记录2016/JS3运动基础/moveperfect.js
@@ -0,0 +1,58 @@
+
+    function getStyle(obj,name)
+    {
+        if(obj.currentStyle)
+        {
+             return obj.currentStyle[name]; 
+        }
+        else
+        {
+            return getComputedStyle(obj,false)[name];
+        }
+    }
+
+    //srartMove(oDiv,{width:400,height:300});
+    function startMove(obj,json,fnEnd)
+    {
+        clearInterval(obj.timer);
+        obj.timer=setInterval(function()
+        {
+            var bStop=true;//假设所有的目标值都达到了
+            for(var attr in json)
+            {
+                var cur=0;
+                if(attr=='opacity')
+                {
+                    cur=Math.round(parseFloat(getStyle(obj,attr))*100);
+                }
+                else
+                {
+                    cur=parseInt(getStyle(obj,attr));
+                }
+
+                //json[attr]相当于当前循环的那个target
+                var speed=(json[attr]-cur)/6;
+                speed=speed>0?Math.ceil(speed):Math.floor(speed);
+
+                if(cur!=json[attr])
+                  bStop=false;
+                if(attr=='opacity')
+                {
+                    obj.style.filter='alpha(opacity:'+(cur+speed)+')';
+                    obj.style.opacity=(cur+speed)/100;
+                }
+                else
+                {
+                    obj.style[attr]=cur+speed+'px';
+                }
+            }
+
+            if(bStop)
+            {
+              clearInterval(obj.timer);
+
+              if(fnEnd)fnEnd();
+            }
+
+        },30);
+    }
